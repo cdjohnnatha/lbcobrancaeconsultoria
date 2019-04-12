e2b5e75f38874a34d5eee2aeda43c529d2bf03c7
cdjohnnatha <cdjohnnatha@gmail.com>
2019-04-12 14:29:03 -0300

    create users Crud

--- config/routes/v1/users.js
+++ config/routes/v1/users.js
@@ -1,10 +1,14 @@
 const express = require('express');
 
 const router = express.Router();
-const { index, show, create } = require('../../../lib/api/controllers/v1/usersController');
+const {
+  index, show, create, destroy, update,
+} = require('../../../lib/api/controllers/v1/usersController');
 
 router.get('/', index);
 router.get('/:id', show);
 router.post('/', create);
+router.patch('/:id', update);
+router.delete('/:id', destroy);
 
 module.exports = router;
